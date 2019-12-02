282359b333c657544ac71ed36b730de6ebf644b8
vvvladimirrrr <vvvladimirrrr@mail.ru>
2019-12-02 23:59:20 +0400

    include gulp-cssmin

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,14 @@ gulp.task('browser-sync', function() {
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
 });
+
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+ 
+gulp.task('css-min', function (done) {
+    gulp.src('src/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist'));
+        done();
+});
