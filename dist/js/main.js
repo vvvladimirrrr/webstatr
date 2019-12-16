d7b5a09b86732d8991202b90db89e6edd2ceb49f
vvvladimirrrr <vvvladimirrrr@mail.ru>
2019-12-16 21:53:51 +0400

    feat: jquery

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,4 @@
+/*
 document.addEventListener("DOMContentLoaded", function(event) { 
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
@@ -11,5 +12,34 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
   closeBtn.addEventListener('click', switchModal)
+
+});
+*/
+$(document).ready(function(){
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function () {
+      modal.toggleClass('modal--visible');
+  }); 
+  closeBtn.on('click',function () {
+      modal.toggleClass('modal--visible');
+  });
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+        $('.button-up').fadeIn();
+    } else {
+        $('.button-up').fadeOut();
+    }
+  });
+    
+  $('.button-up').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
+  });
 
 });
