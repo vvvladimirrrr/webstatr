1dfff7e53dfc234682fc3802ed53d319fac5da70
vvvladimirrrr <vvvladimirrrr@mail.ru>
2019-12-18 19:54:24 +0400

    feat: add projects and slider

--- dist/js/main.js
+++ dist/js/main.js
@@ -42,4 +42,24 @@ $(document).ready(function(){
     return false;
   });
 
+  var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left',prev.width() + 10 + bullets.width() + 10);
+  bullets.css('left',prev.width() + 10)
+
 });
