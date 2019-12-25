f6032da4aa1d4f6f2f037b05b1999a07b23e0401
vvvladimirrrr <vvvladimirrrr@mail.ru>
2019-12-25 20:22:18 +0400

    feat: add map

--- dist/js/main.js
+++ dist/js/main.js
@@ -98,4 +98,40 @@ $(document).ready(function(){
   //маска для телефона
   $('[type=tel]').mask('+7(000) 000-00-00',{placeholder: "+7 (__) ___-__-__"});
 
+  //сощдание карты
+
+ymaps.ready(function () {
+  var myMap = new ymaps.Map('map', {
+          center: [55.786786, 49.142331],
+          zoom: 17
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+
+      // Создаём макет содержимого.
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+          '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      ),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          hintContent: 'Собственный значок метки',
+          balloonContent: 'Это красивая метка'
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: 'dist/img/icon-map.png',
+          // Размеры метки.
+          iconImageSize: [32, 32],
+          // Смещение левого верхнего угла иконки относительно
+          // её "ножки" (точки привязки).
+          iconImageOffset: [-5, -38]
+      })
+
+  myMap.geoObjects
+    .add(myPlacemark)
+});
+
+
 });
